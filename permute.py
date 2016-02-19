3ef6d0c1a9718429795edfb5d3defca710f6f08a
Leong Yew Long <yewlong42@gmail.com>
2016-02-19 16:47:41 +0800

    add estimate for running time. sort answers before printing for easy analysis

--- permute.py
+++ permute.py
@@ -1,8 +1,46 @@
 #!/usr/bin/python
+DEBUG=False
+
+#estimates the number of operations = n! / r! q! for each r and q that has repetition
+def estimateRuntime(mystr):
+	n = len(mystr)
+	numops = fact(n)
+	mystrsorted = sorted(mystr)
+
+	prevchar = mystrsorted[0]
+	count = 1
+	for i in range(1, n):
+		mychar = mystrsorted[i]
+		if prevchar == mychar:
+			count = count + 1
+		else:
+			numops = numops / fact(count)
+			count = 1
+		prevchar = mychar
+	numops = numops / fact(count)
+	# 350k ops per sec  for high number of ops
+	speed_highops = 350000.0
+	# 110k ops per sec  for small number of ops
+	speed_lowops = 110000.0
+	# 200k ops and below are considered small number of ops
+	lowops_threshold = 200000
+
+	time = 0
+	if numops < lowops_threshold:
+		time = numops / 110000.0
+	else:
+		time = numops / 350000.0
+	return time
+
+def fact(n):
+	if n == 1: return 1
+	else: return (n * fact(n-1))
 
 #returns a list of all permutations of mystr
 def perm(mystr):
-	if len(mystr) == 1: return mystr
+	if DEBUG: print 'permuting: ', mystr
+	if len(mystr) == 1: return set(mystr)
+	if DEBUG: print 'combining: ', mystr[0], ' with permutation of ', mystr[1:]
 	return combine(mystr[0], perm(mystr[1:]))
 
 #given a char and a list of strs, put char into each slot
@@ -14,7 +52,18 @@ def combine(mychar, mystrArr):
 	for mystr in mystrArr:
 		for i in range(len(mystr)+1):
 			currstr = mystr[:i] + mychar + mystr[i:]
-			output.add(currstr)
+			try:
+				output.add(currstr)
+			except Exception as e:
+				print type(e)
+				print e.args
+				print e
+				for arg in e.args:
+					print arg
+				print 'error, currstr is ', currstr
+				print 'size of output is ', len(output)
+				import sys
+				sys.exit()
 	return output
 
 mydict=set()
@@ -37,10 +86,12 @@ def solveOne(clue):
 #e.g. if the solution is "took a gander" then we can call 
 # solve("agtdrkaoone", [4, 5]) 
 def solve(clue, spaces):
+	print 'estimated running time: ', estimateRuntime(clue) ,' seconds'
 	spaces.insert(0, 0)
 	spaces.append(len(clue)+1)
 	if len(mydict) == 0: readDict()
 	myperms = perm(clue.lower())
+	answers = set()
 	for candidate in myperms:
 		matches = 0
 		for i in range(len(spaces)-1):
@@ -52,6 +103,13 @@ def solve(clue, spaces):
 			for i in range(len(spaces)-1):
 				currCandidate = candidate[spaces[i]:(spaces[i+1])]
 				toprint = toprint + currCandidate + ' '
-			print toprint
+			#print toprint
+			answers.add(toprint)
 
-# solve('agtdrkaoone', [4,5])
+	for myans in sorted(answers):
+		print myans
+
+solve("agtdrkaoone", [4, 5]) 
+solve('pieceachance', [5,6])
+solve('loadsoffun', [5,7])
+solve('camellot', [5])
